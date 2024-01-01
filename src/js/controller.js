befe811dc21ac1ed47e389da4fb693ebf10bb07c
zumiokh <khantarkaswong24@gmail.com>
2024-01-01 18:47:48 +0700

    Initial Commit

--- src/js/controller.js
+++ src/js/controller.js
@@ -136,7 +136,6 @@ const init = function () {
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
   console.log('Welcome');
-  console.log('BUG');
 };
 
 init();
